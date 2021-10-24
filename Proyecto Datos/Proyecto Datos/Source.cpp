4e64b0283fa4ee04ae4bf4644bad250cd67fcbf0
Angelo0512 <murillocalvo342@gmail.com>
2021-10-24 16:12:11 -0600

    Tiene o no tiene?

--- Proyecto Datos/Proyecto Datos/Source.cpp
+++ Proyecto Datos/Proyecto Datos/Source.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include "DoubleList.h"
 #include "PriorityQueue.h"
+#include "Usuario.h"
+#include <stdlib.h>
+#include <time.h>
 
 /*
 	Estudiantes:
@@ -19,57 +22,96 @@ Enlaces visitados:
 	https://www.geeksforgeeks.org/priority-queue-using-doubly-linked-list/
 	https://www.youtube.com/watch?v=cuL8gXCSA58
 */
+/*
+	Una pagina los usuarios hacen fila para darle uso
+	Sin embargo, la pagina deja ingresar a los usuarios en un orden de prioridad
+	Por lo tanto lo usarios tiene una prioridad asignada
+*/
 using namespace std;
 
+template<typename T>
+void mostrarColaUsuario(PriorityQueue<T> *cola) {
+	while (!cola->isEmpty()) {
+		Usuario newUser = cola->dequeue();
+		std::cout <<  newUser << std::endl;
+	}
+}
+
 int main() {
+	srand(time(NULL));
 
-	// Crear cola de prioridad de enteros
-	PriorityQueue<int>* p = new PriorityQueue<int>(false);
+	//Se crean usuarios con una prioridad random del 1 al 10
+	Usuario A((rand() % (10 - 1 + 1) + 1), 'A');
+	Usuario B((rand() % (10 - 1 + 1) + 1), 'B');
+	Usuario C((rand() % (10 - 1 + 1) + 1), 'C');
+	Usuario D((rand() % (10 - 1 + 1) + 1), 'D');
+	Usuario E((rand() % (10 - 1 + 1) + 1), 'E');
 
-	// Añadir unos cuantos números
-	p->enqueue(2);
-	p->enqueue(5);
-	p->enqueue(1);
-	p->enqueue(4);
-	p->enqueue(12);
-	p->enqueue(4);
-	p->enqueue(6);
-	p->enqueue(5);
+	//Se crea una cola de prioridad con prioridad mayor(constructor por default)
+	PriorityQueue<Usuario>* colaUsuarios = new PriorityQueue<Usuario>();
+
+	//Se agregan los usuarios a la cola de prioridad
+	colaUsuarios->enqueue(A);
+	colaUsuarios->enqueue(B);
+	colaUsuarios->enqueue(C);
+	colaUsuarios->enqueue(D);
+	colaUsuarios->enqueue(E);
 
 	// Imprime y saca de la cola de prioridad
 	// Maneja las posibles excepciones
+	std::cout << "--Se imprime la cola con prioridad mayor--\n";
 	try {
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
-		cout << p->dequeue() << endl;
+		mostrarColaUsuario(colaUsuarios);
 	}
 	catch (exception e) {
 		cout << e.what() << endl;
 	}
 
-	// la cola está vacía, añadir un par de números 
-	p->enqueue(1);
-	p->enqueue(3);
+	//Se reinsertan los elementos a la cola
+	colaUsuarios->enqueue(A);
+	colaUsuarios->enqueue(B);
+	colaUsuarios->enqueue(C);
+	colaUsuarios->enqueue(D);
+	colaUsuarios->enqueue(E);
 
-	// Construye una cola de prioridad copia
-
-	PriorityQueue<int>* q = new PriorityQueue<int>(p);
-
-	// Imprime los elementos que se copiaron 
+	PriorityQueue<Usuario>* colaCopia = new PriorityQueue<Usuario>(*colaUsuarios);
+	// Imprime y saca de la cola de prioridad
+	// Maneja las posibles excepciones
+	std::cout << "\n--Se imprime la cola de prioridad copia--\n";
 	try {
-		cout << q->dequeue() << endl; // NO SE COPIAN LOS ELEMENTOS DE LA LISTA 
-		cout << q->dequeue() << endl;
-		cout << q->dequeue() << endl;
+		mostrarColaUsuario(colaUsuarios);
 	}
 	catch (exception e) {
 		cout << e.what() << endl;
 	}
+
+	//Se crea una listaDoble
+	DoubleList<Usuario>* listaUsuario = new DoubleList<Usuario>();
+
+	//Se agregan los usuario a la lista
+	listaUsuario->add(A);
+	listaUsuario->add(B);
+	listaUsuario->add(C);
+	listaUsuario->add(D);
+	listaUsuario->add(E);
+
+	//Se crea la cola de prioridad a partir de la lista
+	PriorityQueue<Usuario>* colaLista = new PriorityQueue<Usuario>(listaUsuario, false); //Se indica el orden menor
+
+	// Imprime y saca de la cola de prioridad
+	// Maneja las posibles excepciones
+	std::cout << "\n--Se imprime la cola de prioridad a partir de una lista--\n";
+	try {
+		mostrarColaUsuario(colaLista);
+	}
+	catch (exception e) {
+		cout << e.what() << endl;
+	}
+
+	//Elimina la cola de prioridad
+	delete colaUsuarios;
+	delete colaCopia;
+	delete colaLista;
 
 	return 0;
 }
--- Proyecto Datos/Proyecto Datos/Usuario.h
+++ Proyecto Datos/Proyecto Datos/Usuario.h
@@ -1,6 +1,8 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+#include <sstream>
+
 class Usuario {
 private:
 	int prioridad;
@@ -20,7 +22,22 @@ public:
 
 	//Sobrecarga del operador <, compara si la prioridad es menor
 	bool operator < (Usuario const& u) { return prioridad < u.prioridad ? true : false; }
-};
+
+	//Sobrecarga el operador ==, compara si la prioridad es igual
+	bool operator == (Usuario const& u) {
+		return prioridad == u.prioridad ? true : false;
+	}
+	std::string toString() {
+		std::stringstream s;
+		s << "Usuario:\tId: " << id << "\tPrioridad: " << prioridad;
+		return s.str();
+	}
+
+};	
+
+std::ostream& operator<<(std::ostream& out, Usuario& u) {
+	return out << u.toString();
+}
 #endif // !USUARIO_H
 
 
